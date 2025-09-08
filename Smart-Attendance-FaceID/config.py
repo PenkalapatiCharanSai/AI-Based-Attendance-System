d451c5c780ba73690fd39ea0f1fd5180ad194a45
PenkalapatiCharanSai <158132538+PenkalapatiCharanSai@users.noreply.github.com>
2025-09-08 12:11:51 +0530

    config.py

--- Smart-Attendance-FaceID/config.py
+++ Smart-Attendance-FaceID/config.py
@@ -5,9 +5,10 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "mysecretkey"
 
     # MongoDB connection
-    MONGO_URI = os.environ.get("MONGO_URI") or "mongodb+srv://charan:charan@cluster0.zmg8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_URI = os.environ.get("MONGO_URI") or "mongodb://localhost:27017/"
 
     # Database & Collection names
     DB_NAME = "attendance_db"
     USERS_COLLECTION = "users"
     ATTENDANCE_COLLECTION = "attendance"
+
